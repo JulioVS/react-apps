6a76a437644c165f37acde7f71766d9a0c4d2a89
JulioVS <julioer@me.com>
2024-04-23 14:17:03 -0400

    Formatted data into table

--- src/App.js
+++ src/App.js
@@ -18,21 +18,39 @@ const App = (props)=> {
             });
     }, []);
 
-    // Rendering the list of breeds
+    // Internal CSS for table and cell borders
+    const tableCellStyle = {
+        border: '1px solid black'
+    };
+
+    // Rendering the list of breeds in a table format
     return (
         <div>
             <h2>List of Dog Breeds</h2>
-            <ul>
-                {breeds.map((breed, index) => (
-                    <li key={index}>
-                        <h3>{breed.attributes.name}</h3>
-                        <p>Life Span: {breed.attributes.life.min} - {breed.attributes.life.max} years</p>
-                        <p>Male Weight: {breed.attributes.male_weight.min} - {breed.attributes.male_weight.max} kg</p>
-                        <p>Female Weight: {breed.attributes.female_weight.min} - {breed.attributes.female_weight.max} kg</p>
-                        <p>Hypoallergenic: {breed.attributes.hypoallergenic ? 'Yes' : 'No'}</p>
-                    </li>
-                ))}
-            </ul>
+            <table style={{borderCollapse: 'collapse', width: '100%', border: '2px solid black'}}>
+                <thead>
+                    <tr>
+                        <th style={tableCellStyle}>Name</th>
+                        {/* <th style={tableCellStyle}>Description</th> */}
+                        <th style={tableCellStyle}>Life Span (years)</th>
+                        <th style={tableCellStyle}>Male Weight (kg)</th>
+                        <th style={tableCellStyle}>Female Weight (kg)</th>
+                        <th style={tableCellStyle}>Hypoallergenic</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {breeds.map((breed, index) => (
+                        <tr key={index}>
+                            <td style={tableCellStyle}>{breed.attributes.name}</td>
+                            {/* <td style={tableCellStyle}>{breed.attributes.description}</td> */}
+                            <td style={tableCellStyle}>{breed.attributes.life.min} - {breed.attributes.life.max}</td>
+                            <td style={tableCellStyle}>{breed.attributes.male_weight.min} - {breed.attributes.male_weight.max}</td>
+                            <td style={tableCellStyle}>{breed.attributes.female_weight.min} - {breed.attributes.female_weight.max}</td>
+                            <td style={tableCellStyle}>{breed.attributes.hypoallergenic ? 'Yes' : 'No'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
